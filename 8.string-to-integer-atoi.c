4987e7d28a7aeebe1e0bbd373669b1ef761423d1
Soulike <316413310@qq.com>
2019-06-24 10:32:44 +0800

    8

---
+++ 8.string-to-integer-atoi.c
@@ -0,0 +1,155 @@
+/*
+ * @lc app=leetcode id=8 lang=c
+ *
+ * [8] String to Integer (atoi)
+ *
+ * https://leetcode.com/problems/string-to-integer-atoi/description/
+ *
+ * algorithms
+ * Medium (14.65%)
+ * Likes:    978
+ * Dislikes: 6142
+ * Total Accepted:    376.4K
+ * Total Submissions: 2.6M
+ * Testcase Example:  '"42"'
+ *
+ * Implement atoi which converts a string to an integer.
+ * 
+ * The function first discards as many whitespace characters as necessary until
+ * the first non-whitespace character is found. Then, starting from this
+ * character, takes an optional initial plus or minus sign followed by as many
+ * numerical digits as possible, and interprets them as a numerical value.
+ * 
+ * The string can contain additional characters after those that form the
+ * integral number, which are ignored and have no effect on the behavior of
+ * this function.
+ * 
+ * If the first sequence of non-whitespace characters in str is not a valid
+ * integral number, or if no such sequence exists because either str is empty
+ * or it contains only whitespace characters, no conversion is performed.
+ * 
+ * If no valid conversion could be performed, a zero value is returned.
+ * 
+ * Note:
+ * 
+ * 
+ * Only the space character ' ' is considered as whitespace character.
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. If the numerical
+ * value is out of the range of representable values, INT_MAX (2^31 − 1) or
+ * INT_MIN (−2^31) is returned.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "42"
+ * Output: 42
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "   -42"
+ * Output: -42
+ * Explanation: The first non-whitespace character is '-', which is the minus
+ * sign.
+ * Then take as many numerical digits as possible, which gets 42.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "4193 with words"
+ * Output: 4193
+ * Explanation: Conversion stops at digit '3' as the next character is not a
+ * numerical digit.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "words and 987"
+ * Output: 0
+ * Explanation: The first non-whitespace character is 'w', which is not a
+ * numerical 
+ * digit or a +/- sign. Therefore no valid conversion could be performed.
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "-91283472332"
+ * Output: -2147483648
+ * Explanation: The number "-91283472332" is out of the range of a 32-bit
+ * signed integer.
+ * Thefore INT_MIN (−2^31) is returned.
+ * 
+ */
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+void ltrim(char *s)
+{
+    int len = strlen(s);
+    if(len == 0)
+    {
+        return;
+    }
+    int index = 0; // 从哪个下标开始不是空格
+    for (int i = 0; i <= len; i++)
+    {
+        if (s[i] != ' ')
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if(index == 0)
+    {
+        return;
+    }
+
+    int trimLen = len - index;
+    // 原地修改，不是太好的习惯，但可以提高速度
+    memmove(s, s + index, trimLen * sizeof(char));
+    s[trimLen] = '\0';
+}
+
+int myAtoi(char *str)
+{
+    ltrim(str);
+    long long sum = 0;
+    int len = strlen(str);
+    bool isPositive = str[0] != '-';
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i == 0 && (str[i] == '+' || str[i] == '-'))
+        {
+            continue;
+        }
+
+        if (!isdigit(str[i]))
+        {
+            break;
+        }
+
+        sum *= 10;
+        sum += str[i] - '0';
+
+        if (isPositive && sum > INT32_MAX)
+        {
+            return INT32_MAX;
+        }
+        else if (!isPositive && -1 * sum <= INT32_MIN)
+        {
+            return INT32_MIN;
+        }
+    }
+
+    return (isPositive? 1 : -1) * sum;
+}
